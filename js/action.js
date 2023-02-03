56448bcf361cb130fb3729a7bbe545220df4d2cb
llSaiMonDll <zerotwotv@gmail.com>
2023-02-03 12:31:58 +0700

    NameCard-2

---
+++ js/action.js
@@ -0,0 +1,6 @@
+window.alert("Welcome to my Website");
+const myInfo = {fistName: "Kampol" , lastname: "Suwannatam" , Status: "Student", ID: "65130500201"}
+
+for(i in myInfo){
+    console.log(myInfo[i]);
+}
